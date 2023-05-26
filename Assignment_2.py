afcf455e9d3b4011fd0bcb5e5c913af2055dd680
Ashishdas31 <116709291+Ashishdas31@users.noreply.github.com>
2023-05-26 17:24:11 +0530

    Add files via upload

---
+++ Assignment_2.py
@@ -0,0 +1,24 @@
+# Ans.  8.
+spam =1
+
+if spam ==1:
+    print("hello")
+elif spam==2:
+    print("Howdy")
+else:
+    print("Greetings!")
+
+print("")
+
+# Ans.  12.
+for i in range(0,11):
+    print(i)
+
+print("")
+
+a=0
+
+while a<11:
+    print(a)
+    a+=1
+
